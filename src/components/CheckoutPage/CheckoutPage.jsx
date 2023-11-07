9bdffe0a35b9405ff0b926b63ad5ea2f4c7c8e0f
virender <virender.digitalshakha@gmail.com>
2023-11-08 03:57:07 +0530

    changes

--- src/components/CheckoutPage/CheckoutPage.jsx
+++ src/components/CheckoutPage/CheckoutPage.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, {  useState } from 'react';
 import { useFormik } from 'formik';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
